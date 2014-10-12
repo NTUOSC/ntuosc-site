d9c67c214b9973121854dfa7ce6a022d0b36df0a
Poren Chiang <ren.chiang@gmail.com>
2014-10-13 03:05:54 +0800

    Implementing base theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,26 +3,28 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'臺大開源社'
-SITENAME = u'臺灣大學開源社'
+SITENAME = u'臺灣大學開源社 / NTU Open Source Community'
+SITESUBTITLE = '讓我們一起推廣開源理念，許世界更美好的未來！'
 SITEURL = ''
 
 TIMEZONE = 'Asia/Taipei'
 
-DEFAULT_LANG = u'tw'
+DEFAULT_LANG = u'zh-TW'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
-LINKS =  []
-SOCIAL = []
-
 DEFAULT_PAGINATION = 3
 
 # Resources
 STATIC_PATHS = ['images', 'vendor']
+THEME = 'ntuosc'
+
+# Metadata
+LINKS =  []
+SOCIAL = []
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
