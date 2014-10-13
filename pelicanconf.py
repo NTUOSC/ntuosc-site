ed003bce45515365275f6625e9441da9d44ba29c
Poren Chiang <ren.chiang@gmail.com>
2014-10-14 00:13:12 +0800

    Add template prototype and configure URL

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,11 +16,40 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-DEFAULT_PAGINATION = 3
-
 # Resources
+PATH = 'content'
 STATIC_PATHS = ['images', 'vendor']
-THEME = 'ntuosc'
+THEME = 'themes/ntuosc'
+
+# Alter template behavior
+DIRECT_TEMPLATES = ('index', 'archives')
+PAGINATED_DIRECT_TEMPLATES = ('archives',)
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{slug}/'
+ARTICLE_SAVE_AS = ARTICLE_URL + 'index.html'
+ARTICLE_LANG_URL = ARTICLE_URL + '{lang}/'
+ARTICLE_LANG_SAVE_AS = ARTICLE_LANG_URL + 'index.html'
+
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = PAGE_URL + 'index.html'
+PAGE_LANG_URL = PAGE_URL + '{lang}/'
+PAGE_LANG_SAVE_AS = PAGE_LANG_URL + 'index.html'
+
+ARCHIVES_URL = 'posts/'
+ARCHIVES_SAVE_AS = 'posts/index.html'
+
+CATEGORIES_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+TAGS_SAVE_AS = ''
+TAG_SAVE_AS = ''
+AUTHORS_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
+
 
 # Metadata
 LINKS =  []
